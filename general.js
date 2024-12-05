3cfcab48cd82e4ccae208e98d0e61bcd9781f914
Humoresque <pdiscordgeneral@gmail.com>
2024-12-04 21:22:41 -0800

    Dev 6

--- general.js
+++ general.js
@@ -1,30 +1,34 @@
 let speedrunTableBody = document.getElementById("speedrunTable").children[0]
 
 try {
-    let speedrunData = JSON.parse(fetchJSONData("./data/speedruns.json"))
-    console.log(speedrunData["speedruns"].length)
+    fetchJSONData("./data/speedruns.json")
 }
 catch {
     console.error("Failed to extract JSON")
 }
 
 
-for (let i = 1; i < speedrunTableBody.children.length; i++) {
-    let timeVar = speedrunTableBody.children[i].children[1]
-    timeVar.textContent = "test"
-}
 
 function fetchJSONData(path) {
     fetch(path)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error
-                    (`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) =>
-            console.log(data))
-        .catch((error) =>
-            console.error("Unable to fetch data:", error));
+        .then((response) => response.json())
+        .then((json) => UpdateSpeedrunText(json));
+}
+
+function UpdateSpeedrunText(json) {
+
+    let max = speedrunTableBody.children.length
+    if (json.speedruns.length < speedrunTableBody.children.length) {
+        max = json.speedruns.length
+    }
+
+    for (let i = 0; i < max; i++) {
+        let timeVar = speedrunTableBody.children[i + 1].children[1]
+        let userVar = speedrunTableBody.children[i + 1].children[2]
+
+        minutes = Math.floor(Number(json.speedruns[i].time) / 60)
+
+        timeVar.textContent = minutes + ":" + Number(json.speedruns[i].time) % 60
+        userVar.textContent = json.speedruns[i].users
+    }
 }
