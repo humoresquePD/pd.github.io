4f5975daedd41b3fe1bd447028dc5c6bfcc6f56a
Humoresque <pdiscordgeneral@gmail.com>
2025-12-04 16:46:24 -0800

    fix issue

--- general.js
+++ general.js
@@ -2,7 +2,6 @@
 var statsDisplayed = []
 var defaultPeopleList = ["Dinoo", "Humoresque811", "卄anu卄al", "Lilith", "NoxiRad"]
 
-var speedrunTableBody = document.getElementById("speedrunTable").children[0]
 var ratioTable = document.getElementById("ratioTable")
 
 var form = document.getElementById("ratio_adder")
@@ -25,24 +24,6 @@ function fetchJSONData(path) {
     fetch(path)
         .then((response) => response.json())
         .then((json) => UpdateSpeedrunText(json));
-}
-
-function UpdateSpeedrunText(json) {
-
-    let max = speedrunTableBody.children.length
-    if (json.speedruns.length < speedrunTableBody.children.length) {
-        max = json.speedruns.length
-    }
-
-    for (let i = 0; i < max; i++) {
-        let timeVar = speedrunTableBody.children[i + 1].children[1]
-        let userVar = speedrunTableBody.children[i + 1].children[2]
-
-        minutes = Math.floor(Number(json.speedruns[i].time) / 60)
-
-        timeVar.textContent = minutes + ":" + Number(json.speedruns[i].time) % 60   
-        userVar.textContent = json.speedruns[i].users
-    }
 }
 
 function AddPersonToRatioRankings(user) {
@@ -76,7 +57,7 @@ function GetFurtherDataOnUser(data1) {
         AddNewUserRow(data1, data)
     }).catch(function (error) {
         console.error(error)
-        alert("Invalid Username 2")
+        alert("User doesn't exist")
     })
 }
 
